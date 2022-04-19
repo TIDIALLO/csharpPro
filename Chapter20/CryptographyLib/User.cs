57f715a2b8a29e251c438a8e055e6ba5543ba441
tidianediallo <diallotidiane@gmail.com>
2022-04-19 01:46:38 -0100

    ajout du chapitre 20 Protecting Your Data and Applications

---
+++ Chapter20/CryptographyLib/User.cs
@@ -0,0 +1,18 @@
+namespace Packt.Shared;
+
+public class User
+{
+    public string Name { get; set; }
+    public string Salt { get; set; }
+    public string SaltedHashedPassword { get; set; }
+
+    public string[]? Roles { get; set; }
+
+    public User(string name, string salt, string saltedHashedPassword,string[] roles)
+    {
+        Name = name;
+        Salt = salt;
+        SaltedHashedPassword = saltedHashedPassword;
+        Roles = roles;
+    }
+}
